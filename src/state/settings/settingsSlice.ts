602a6ed4858820c3f7fe924795bc43b5b112627f
James Francis <jamesfrancis2001@outlook.com>
2025-01-25 09:06:22 +0000

    refactor settings slice; update state properties and add toggle actions for score and player management settings

--- src/state/settings/settingsSlice.ts
+++ src/state/settings/settingsSlice.ts
@@ -3,36 +3,59 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 export interface SettingsState {
     theme: string;
     hideTotalScore: boolean;
-    disableScoreEdit: boolean;
-    disableScoreLimit: boolean;
+    enableScoreEdit: boolean;
+    enableScoreLimit: boolean;
+    enableScoreReset: boolean;
+    enablePlayersReset: boolean;
 }
 
-const initialState: SettingsState = {
-    theme: 'light',
-    hideTotalScore: false,
-    disableScoreEdit: false,
-    disableScoreLimit: false,
-};
+const loadInitialState = (): SettingsState => {
+    const storedSettings = localStorage.getItem('settings');
+    return storedSettings
+        ? JSON.parse(storedSettings)
+        : {
+            enablePlayersReset: false,
+            enableScoreEdit: true,
+            enableScoreLimit: true,
+            enableScoreReset: true,
+            hideTotalScore: false,
+            theme: 'dark',
+        };
+}
+
+const initialState: SettingsState = loadInitialState();
 
 const settings = createSlice({
     name: 'settings',
     initialState,
     reducers: {
         setTheme: (state: SettingsState, action: PayloadAction<string>) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, theme: action.payload }));
             state.theme = action.payload;
         },
         toggleHideTotalScore: (state: SettingsState) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, hideTotalScore: !state.hideTotalScore }));
             state.hideTotalScore = !state.hideTotalScore;
         },
-        toggleDisableScoreEdit: (state: SettingsState) => {
-            state.disableScoreEdit = !state.disableScoreEdit;
+        toggleEnableScoreEdit: (state: SettingsState) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, enableScoreEdit: !state.enableScoreEdit }));
+            state.enableScoreEdit = !state.enableScoreEdit;
         },
-        toggleDisableScoreLimit: (state: SettingsState) => {
-            state.disableScoreLimit = !state.disableScoreLimit;
+        toggleEnableScoreLimit: (state: SettingsState) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, enableScoreLimit: !state.enableScoreLimit }));
+            state.enableScoreLimit = !state.enableScoreLimit;
+        },
+        toggleEnableScoreReset: (state: SettingsState) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, enableReset: !state.enableScoreReset }));
+            state.enableScoreReset = !state.enableScoreReset;
+        },
+        toggleEnablePlayersReset: (state: SettingsState) => {
+            localStorage.setItem('settings', JSON.stringify({ ...state, enablePlayersReset: !state.enablePlayersReset }));
+            state.enablePlayersReset = !state.enablePlayersReset;
         },
     },
 });
 
 
-export const { setTheme, toggleHideTotalScore, toggleDisableScoreEdit, toggleDisableScoreLimit } = settings.actions;
+export const { setTheme, toggleHideTotalScore, toggleEnableScoreEdit, toggleEnableScoreLimit, toggleEnablePlayersReset, toggleEnableScoreReset } = settings.actions;
 export default settings.reducer;
