42ce1e8616b177c2f837bbdb5633ec5c90a3f563
James Francis <jamesfrancis2001@outlook.com>
2025-02-19 13:49:16 +0000

    add SpeedInsights component; integrate performance monitoring tool into the application

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
 import { store } from './state/store.ts'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <SpeedInsights />
     </Provider>
   </StrictMode>,
 )
